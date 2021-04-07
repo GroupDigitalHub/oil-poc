6b9686c23f5cf985cfa637e68b985e0f3cf78c38
markbok <62894504+markbok@users.noreply.github.com>
2021-04-07 23:08:25 +0100

    Add react-azure-adb2c

---
+++ src/components/Authentication/index.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+// import authentication from 'react-azure-adb2c';
+// import Homepage from '../components/Homepage';
+
+
+// // ADB2C
+// authentication.initialize({
+//   instance: 'https://login.microsoftonline.com/tfp/', 
+//   tenant: 'OILtenant.onmicrosoft.com',
+//   signInPolicy: 'B2C_1_signupsignin1',
+//   applicationId: ' e40c0c3a-c280-4b44-a8f5-299d2d68f44d',
+//   cacheLocation: 'sessionStorage',
+//   scopes: [],
+//   redirectUri: 'http://localhost:3000',
+//   postLogoutRedirectUri: window.location.origin,
+// });
+
+
+// markup
+const IndexPage = Page => {
+  return (
+      <Page />
+  )
+}
+
+export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,27 @@
 import * as React from "react"
+import authentication from 'react-azure-adb2c';
 import Homepage from '../components/Homepage';
 
-// styles
+
+// // ADB2C
+authentication.initialize({
+  instance: 'https://login.microsoftonline.com/tfp/', 
+  tenant: 'OILtenant.onmicrosoft.com',
+  signInPolicy: 'B2C_1_signupsignin1',
+  applicationId: '0325a5d0-c15e-40a1-880a-65fee6155dc4',
+  cacheLocation: 'sessionStorage',
+  scopes: ['https://OILtenant.onmicrosoft.com/react-poc/user_impersonation'],
+  redirectUri: 'http://localhost:8000',
+  postLogoutRedirectUri: window.location.origin,
+});
 
 
 // markup
 const IndexPage = () => {
-  return (
-      <Homepage />
-  )
+  return authentication.run(() => {
+    <Homepage />
+  });
+  // return authentication.required(Homepage)
 }
 
 export default IndexPage
